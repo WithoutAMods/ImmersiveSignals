5a8d0fabb06250f6c876bbf26cf6a8dec4ae6ab9
WithoutAName25 <withoutaname@withoutaname.eu>
2022-08-23 16:19:20 +0200

    Register KDeferredRegisters to MOD_BUS

--- src/main/kotlin/eu/withoutaname/mod/immersivesignals/setup/Registration.kt
+++ src/main/kotlin/eu/withoutaname/mod/immersivesignals/setup/Registration.kt
@@ -6,13 +6,17 @@ import eu.withoutaname.mod.immersivesignals.signal.block.SignalEntity
 import net.minecraft.tileentity.TileEntityType
 import net.minecraftforge.registries.ForgeRegistries
 import thedarkcolour.kotlinforforge.forge.KDeferredRegister
+import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 object Registration {
 
     private val blocks = KDeferredRegister(ForgeRegistries.BLOCKS, ImmersiveSignals.ID)
     private val tileEntities = KDeferredRegister(ForgeRegistries.TILE_ENTITIES, ImmersiveSignals.ID)
 
-    fun init() {}
+    fun init() {
+        blocks.register(MOD_BUS)
+        tileEntities.register(MOD_BUS)
+    }
 
     val signalBlock by blocks.registerObject("signal") { SignalBlock }
     val signalEntityType by tileEntities.registerObject("signal") {
